6d1cd9817363aef6f32bbc2a86b55cf77652dbec
WindomZ <git.windomz@gmail.com>
2017-07-14 01:51:48 +0800

    feat: add url-gen.js to support cli

--- url-gen.js
+++ url-gen.js
@@ -1,4 +1,28 @@
+#!/usr/bin/env node
 /**
  * Created by WindomZ on 2017/7/9.
  */
 'use strict';
+
+const path = require('path');
+
+const prog = require('caporal');
+
+const urlgen = require('./generator').promise;
+
+prog
+  .version(require('./package.json').version)
+  .argument('[dir]', 'Relative directory path to generate the URL', /^.+$/, '.')
+  .option('-u, --up <level>', 'Query <level> parent directory', prog.INT, 0)
+  .action(function(args, options) {
+    let dir = path.resolve(args.dir || process.cwd());
+    urlgen(dir, options.up)
+      .then(r => {
+        console.log(r);
+      })
+      .catch(e => {
+        console.error(e.message);
+      });
+  });
+
+prog.parse(process.argv);
